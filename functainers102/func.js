ac7d074f18e0e0998a40f6aa1bf96c297e984779
Shaun Smith <shaun.smith@oracle.com>
2018-01-16 14:23:41 -0500

    Convert tabs to spaces.

--- functainers102/func.js
+++ functainers102/func.js
@@ -13,35 +13,36 @@
 // Output, if all goes well, should look like:
 //
 // {
-// 	'body': '{"greeting": "Hello Tom"}',
-// 	'content_type': 'application/json',
-// 	'protocol': {
-// 		'status_code': 200
-// 		}
+//   'body': '{"greeting": "Hello Tom"}',
+//   'content_type': 'application/json',
+//   'protocol': {
+//     'status_code': 200
+//     }
 // }
 
 
+
 var JSONStream = require('JSONStream')
-	, es = require('event-stream')
+  , es = require('event-stream')
 
 // Read JSON from standard input looking for the body property
 // of the incoming request objects, construct a response, and 
 // write that to standard output.
 process.stdin
-	.pipe(
-		// look for root 'body' property
-		JSONStream.parse(['body']))
-	.pipe(
-		es.mapSync(function(body){
-			// body should be an object with a single 'name' property
-			// like: {"name": "Tom"}
-			var message = 'Hello ' + body.name;
-			return JSON.stringify({
-				'body': {greeting: message},
-				'content_type': 'application/json',
-				'protocol': {
-					'status_code': 200
-					}
-				});
-		}))
-	.pipe(process.stdout);
+  .pipe(
+    // look for root 'body' property
+    JSONStream.parse(['body']))
+  .pipe(
+    es.mapSync(function(body){
+      // body should be an object with a single 'name' property
+      // like: {"name": "Tom"}
+      var message = 'Hello ' + body.name;
+      return JSON.stringify({
+        'body': {greeting: message},
+        'content_type': 'application/json',
+        'protocol': {
+          'status_code': 200
+          }
+        });
+    }))
+  .pipe(process.stdout);
